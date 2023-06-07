7cd9ea25fd1043a1c615fb1c599e0104e372ad59
user <wawaper1998@gmail.com>
2023-06-07 14:35:07 +0400

    new_file

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+rand_num = random.randint(1, 100)
+
+print('Hello!! I guessed an integer from 1 to 100, try to guess it')
+
+
+def is_valid(num):
+    if 1 <= num <= 100:
+        return True
+    return False
+
+
+while True:
+    try:
+        req = int(input("Try to input your number: "))
+        if not is_valid(req):
+            print("Your number is out of range (1-100). Try again.")
+        else:
+            break
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+while req != rand_num:
+    if req > rand_num:
+        try:
+            req = int(input("Your number is bigger than mine, try again: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+    else:
+        try:
+            req = int(input("Your number is less than mine, try again: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+print('Congratulations!!! You won!!!')
