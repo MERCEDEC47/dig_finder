78f981db47734c4c6898b709daa34729929f13fe
user <wawaper1998@gmail.com>
2023-06-07 14:40:27 +0400

    add_try_counter_to_main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 rand_num = random.randint(1, 100)
+count_try = 0
 
 print('Hello!! I guessed an integer from 1 to 100, try to guess it')
 
@@ -14,6 +15,7 @@ def is_valid(num):
 while True:
     try:
         req = int(input("Try to input your number: "))
+        count_try += 1
         if not is_valid(req):
             print("Your number is out of range (1-100). Try again.")
         else:
@@ -25,12 +27,14 @@ while req != rand_num:
     if req > rand_num:
         try:
             req = int(input("Your number is bigger than mine, try again: "))
+            count_try += 1
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
     else:
         try:
             req = int(input("Your number is less than mine, try again: "))
+            count_try += 1
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
 
-print('Congratulations!!! You won!!!')
+print('Congratulations!!! You won!!!', 'Try counter:', count_try)
