3001e1334905c70761c48f6502eb3018f331618a
user <wawaper1998@gmail.com>
2023-06-07 14:48:02 +0400

    1

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 import random
 
-rand_num = random.randint(1, 100)
-count_try = 0
 
-print('Hello!! I guessed an integer from 1 to 100, try to guess it')
+def play_game():
+    rand_num = random.randint(1, 100)
+    attempts = 0  # Инициализация счетчика попыток
 
+    print('Hello!! I guessed an integer from 1 to 100, try to guess it')
 
-def is_valid(num):
-    if 1 <= num <= 100:
-        return True
-    return False
+    def is_valid(num):
+        if 1 <= num <= 100:
+            return True
+        return False
+
+    while True:
+        try:
+            req = int(input("Try to input your number: "))
+            if not is_valid(req):
+                print("Your number is out of range (1-100). Try again.")
+            else:
+                attempts += 1  # Увеличение счетчика попыток
+                if req == rand_num:
+                    print('Congratulations!!! You won!!!')
+                    break
+                elif req > rand_num:
+                    print("Your number is bigger than mine.")
+                else:
+                    print("Your number is less than mine.")
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
 
 
 while True:
-    try:
-        req = int(input("Try to input your number: "))
-        count_try += 1
-        if not is_valid(req):
-            print("Your number is out of range (1-100). Try again.")
-        else:
-            break
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-
-while req != rand_num:
-    if req > rand_num:
-        try:
-            req = int(input("Your number is bigger than mine, try again: "))
-            count_try += 1
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-    else:
-        try:
-            req = int(input("Your number is less than mine, try again: "))
-            count_try += 1
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-
-print('Congratulations!!! You won!!!', 'Try counter:', count_try)
+    play_game()
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != "yes":
+        break
